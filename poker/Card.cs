e96ac4141d3c27edeb74b4a3e55206914258d846
daniilvladimirov <dvladimirov@techlab.solutions>
2021-12-13 03:37:46 +0200

    feat(poker): card class, init deck

---
+++ poker/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace poker
+{
+    public class Card
+    {
+        public enum Suits
+        {
+            Diamonds = 0,
+            Hearts = 1,
+            Clubs = 2,
+            Spades = 3
+        }
+        public Suits suit;
+        public int rank;
+        public String rank_value;
+        private int value;
+        private Suits val;
+
+        public Card()
+        {
+            suit = Suits.Diamonds;
+            rank = 0;
+            rank_value = "";
+        }
+        public Card(Suits suit, int rank) {
+            this.suit = suit;
+            this.rank = rank;
+            if (rank > 10) {
+                if (rank == 11) {
+                    this.rank_value = "jack";
+                } else if (rank == 12) {
+                    this.rank_value = "queen";
+                } else if (rank == 13) {
+                    this.rank_value = "king";
+                }
+            } else {
+                this.rank_value = rank.ToString();
+            }
+        }
+    }
+}
--- poker/Program.cs
+++ poker/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace poker
 {
@@ -6,7 +8,27 @@ namespace poker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int bot_count = 2;
+            List<Card> deck = new List<Card>();
+            List<Card> user_deck = new List<Card>();
+            foreach (Card.Suits suit in Enum.GetValues(typeof(Card.Suits)))
+            {
+                foreach (int rank in Enumerable.Range(1, 13))
+                {
+                    deck.Add(new Card(suit, rank));
+                }
+            }
+            Console.WriteLine(deck.Count);
+            Random random = new Random();
+            foreach (int i in Enumerable.Range(1, 5))
+            {
+                int index = random.Next(0, deck.Count - 1);
+                user_deck.Add(deck[index]);
+            }
+            foreach(Card card in user_deck)
+            {
+                Console.WriteLine(card.rank_value);
+            }
         }
     }
 }
